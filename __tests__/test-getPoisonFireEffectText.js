4a00a0e611d79e2df8114b27c7f6fab7f0f05ca9
mkyang <303055758@qq.com>
2015-01-06 19:20:26 +0800

    add getPoisonFireEffectText.js

--- __tests__/test-getPoisonFireEffectText.js
+++ __tests__/test-getPoisonFireEffectText.js
@@ -1,16 +1,16 @@
-jest.dontMock('../src/model/soldier');
-jest.dontMock('../src/model/weapon');
+jest.dontMock('../src/model/poison-fire-effect');
 jest.dontMock('../src/model/player');
 
 describe('poison-fire-effect', function() {
     describe('getPoisonFireEffectText', function() {
         it('should return correct text', function() {
-            var Soldier = require('../src/model/soldier');
-            var Weapon = require('../src/model/weapon');
             var Player = require('../src/model/player');
+            var PoisonFireEffect = require('../src/model/poison-fire-effect');
 
-            var text = getPoisonFireEffectText();
-            expect(text).toBe('李四受到2点毒性伤害, 李四剩余生命：998');
+            var lisi = new Player('普通人', '李四', 1000, 200, '中毒');
+            var poisonFireEffect = new PoisonFireEffect('毒性', 2);
+            var text = poisonFireEffect.getPoisonFireEffectText(lisi);
+            expect(text).toBe('李四受到2点毒性伤害,李四剩余生命:998');
         });
     });
 });
--- src/model/poison-fire-effect.js
+++ src/model/poison-fire-effect.js
@@ -1,13 +1,19 @@
-function PoisonFireEffect(damage) {
+function PoisonFireEffect(name, damage) {
+    this.name = name;
     this.damage = damage;
 }
 
 PoisonFireEffect.prototype.getPoisonFireEffectText = function(player) {
     var result = '';
-    result = player.name + '受到' + this.damage + 
-        李四受到2点毒性伤害, 李四剩余生命：10
-
+    result = player.name + '受到' + this.damage + '点' + this.name + '伤害,' +
+        player.name + '剩余生命:' + this.getEffectDamage(player);
     return result;
 };
 
+PoisonFireEffect.prototype.getEffectDamage = function(player) {
+    return player.hp -= this.damage;
+};
+
 module.exports = PoisonFireEffect;
+
+
