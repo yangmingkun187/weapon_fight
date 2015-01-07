c4ceef01ab13aff7b89cdd24b8d125aa12f21c01
mkyang <303055758@qq.com>
2015-01-07 10:05:08 +0800

    test getAttackText

--- __tests__/test-soldier.js
+++ __tests__/test-soldier.js
@@ -16,4 +16,19 @@ describe('Soldier', function() {
             expect(text).toBe('战士张三用毒剑攻击了普通人李四,李四受到了210点伤害,李四中毒了,李四剩余生命:790\n');
         });
     });
+
+    describe('#getAttackText', function() {
+        it('should return correct text', function() {
+            var Soldier = require('../src/model/soldier');
+            var Weapon = require('../src/model/weapon');
+            var Player = require('../src/model/player');
+            var weapon = new Weapon('毒剑', 10);
+            var lisi = new Player('普通人', '李四', 1000, 200, '中毒');
+            var zhang = new Soldier('战士', '张三', 1000, 200, '', weapon);
+
+            var text = zhang.getAttackText(lisi);
+            expect(text).toBe('战士张三用毒剑攻击了普通人李四,李四受到了210点伤害,李四剩余生命:790\n');
+
+        });
+    });
 });
