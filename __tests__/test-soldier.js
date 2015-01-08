e3552ce17c3746647b5eb24a704ddda84324b351
mkyang <303055758@qq.com>
2015-01-08 20:59:41 +0800

    refactor test-getSoldierAttackText

--- __tests__/test-soldier.js
+++ __tests__/test-soldier.js
@@ -22,6 +22,16 @@ describe('Soldier', function() {
             var text = zhang.getSoldierAttackText(lisi);
             expect(text).toBe('战士张三用毒剑攻击了普通人李四,李四受到了210点伤害,李四中毒了,李四剩余生命:1000\n');
         });
+
+        it('should return correct text', function() {
+            var effect = new PoisonFireEffect('毒性', 2, 2, 0, '中毒');
+            var weapon = new Weapon('毒剑', 10, effect);
+            var lisi = new Player('普通人', '李四', 1000, 200, '中毒');
+            var zhang = new Soldier('战士', '张三', 1000, 200, '', weapon);
+
+            var text = zhang.getSoldierAttackText(lisi);
+            expect(text).toBe('战士张三用毒剑攻击了普通人李四,李四受到了210点伤害,李四剩余生命:1000\n');
+        });
     });
 
     describe('#attack', function() {
